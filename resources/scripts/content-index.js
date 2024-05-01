cc605f2b20f310f19d22965ffd6a52d93e1f27a8
Flaubert Henriques <flaubertcontato@gmail.com>
2024-05-01 08:21:07 -0300

    develop: update resources

--- resources/scripts/content-index.js
+++ resources/scripts/content-index.js
@@ -2,40 +2,42 @@
 
 // ============ Step 1
 
-const body = document.querySelector( 'body' );
+window.addEventListener('load', () => {
+  const body = document.querySelector( 'body' );
 
-body.addEventListener('click', ( event ) => {
-  if ( event.target.matches( '#JS-content-index #JS-load-index a' ) ) {
-    event.preventDefault();
+  body.addEventListener('click', ( event ) => {
+    if ( event.target.matches( '#JS-content-index #JS-load-index a' ) ) {
+      event.preventDefault();
+  
+      fetchLoad( event.target.href );
+  
+      window.history.pushState( null, null, event.target.href );
+    }
+  });
 
-    fetchLoad( event.target.href );
+  // ============ Step 2
 
-    window.history.pushState( null, null, event.target.href );
+  async function fetchLoad( url ) {
+    document.querySelector( '#JS-content-index' ).innerHTML   = 'Loading...';
+    const fetchRequest                                        = await fetch( url );
+    const html                                                = await fetchRequest.text();
+  
+    replaceContent( html );
   }
-});
 
-// ============ Step 2
+  // ============ Step 3
 
-async function fetchLoad( url ) {
-  document.querySelector( '#JS-content-index' ).innerHTML   = 'Loading...';
-  const fetchRequest                                        = await fetch( url );
-  const html                                                = await fetchRequest.text();
+  function replaceContent( html ) {
+    const newElement      = document.createElement( 'div' );
+    newElement.innerHTML  = html;
+    const oldContent      = document.querySelector( '#JS-content-index' );
+    const newContent      = newElement.querySelector( '#JS-content-index' );
+    oldContent.innerHTML  = newContent.innerHTML;
+  }
 
-  replaceContent( html );
-}
+  // ============ Step 4
 
-// ============ Step 3
-
-function replaceContent( html ) {
-  const newElement      = document.createElement( 'div' );
-  newElement.innerHTML  = html;
-  const oldContent      = document.querySelector( '#JS-content-index' );
-  const newContent      = newElement.querySelector( '#JS-content-index' );
-  oldContent.innerHTML  = newContent.innerHTML;
-}
-
-// ============ Step 4
-
-window.addEventListener('popstate', () => {
-  fetchLoad( window.location.href );
+  window.addEventListener('popstate', () => {
+    fetchLoad( window.location.href );
+  });
 });
