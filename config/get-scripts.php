6195dcdf95e9a15c7b486607d0a8c5ef615fa832
Flaubert Henriques <flaubertcontato@gmail.com>
2024-05-09 21:57:13 -0300

    main: improvements config and resources

--- config/get-scripts.php
+++ config/get-scripts.php
@@ -11,27 +11,24 @@ function get_scripts()
   $package = current_user_can( 'administrator' ) ? '' : 'minified/';
   $script  = current_user_can( 'administrator' ) ? '.js' : '.min.js';
   $version = current_user_can( 'administrator' ) ? $random : wp_get_theme()->get( 'Version' );
- 
-  switch ( true ) {
-    case is_category():
 
-      wp_enqueue_script( 'content-category', get_template_directory_uri() . '/resources/scripts/' . $package . 'content-category' . $script, ['wp-api'], $version, true );
+  if ( is_category() ) {
+    wp_enqueue_script( 'content-category', get_template_directory_uri() . '/resources/scripts/' . $package . 'content-category' . $script, ['wp-api'], $version, true );
 
-      $content_category = new ContentCategory( get_queried_object()->term_id );
-      $content_category->localize_script_content_category( 'content-category', 'category' );
+    $content_category = new ContentCategory( get_queried_object()->term_id );
 
-    break;
+    $content_category->localize_script_content_category( 'content-category', 'category' );
+  }
   
-    case is_page_template( 'template-custom.php' ) && class_exists( 'ACF' ):
+  else if ( is_page_template( 'template-custom.php' ) && class_exists( 'ACF' ) ) {
+    wp_enqueue_script( 'content-template-custom', get_template_directory_uri() . '/resources/scripts/' . $package . 'content-template-custom' . $script, ['wp-api'], $version, true );
 
-      wp_enqueue_script( 'content-template-custom', get_template_directory_uri() . '/resources/scripts/' . $package . 'content-template-custom' . $script, ['wp-api'], $version, true );
+    $content_custom = new ContentTemplateCustom;
 
-      $content_custom = new ContentTemplateCustom;
-      $content_custom->localize_template_custom( 'content-template-custom', 'contentCustom' );
+    $content_custom->localize_template_custom( 'content-template-custom', 'contentCustom' );
+  }
 
-    break;
-  
-    default:
-      wp_enqueue_script( 'content-index', get_template_directory_uri() . '/resources/scripts/' . $package . 'content-index' . $script, [], $version, true );
+  else {
+    wp_enqueue_script( 'content-index', get_template_directory_uri() . '/resources/scripts/' . $package . 'content-index' . $script, [], $version, true );
   }
 }
