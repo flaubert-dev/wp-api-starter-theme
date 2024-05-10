c61b4cecfd44a4c2383c70d7fb685886eae0e229
Flaubert Henriques <flaubertcontato@gmail.com>
2024-05-09 23:18:27 -0300

    main: new test in single.php

--- single.php
+++ single.php
@@ -1,3 +1,17 @@
-<?php
+<?php 
+  /**
+   * Wait... News will come in this file (This is a free beta version!)
+   * 
+   * get_field() and the_field() Test with ACF JSON: "fields" / "key" ( group_662525e42d687.json )
+   * 
+   * Reference: https://www.advancedcustomfields.com/resources/the_field/#check-if-value-exists
+   * 
+   */
 
-// Wait... News will come in this file (This is a free beta version!)
+if ( get_field( 'field_662525e40e21e' ) ): ?>
+
+  <h1 style="background-color: <?php the_field( 'field_662525e40e21e' ) ?>;">
+    Hello World 
+  </h1>
+
+<?php endif;
